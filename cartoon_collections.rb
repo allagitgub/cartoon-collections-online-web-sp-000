a8cb714a10cdc1b0bf0aae5ea59a2f42285fc85a
Alla Kuzminova <alchik@gmail.com>
2020-09-16 01:36:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(words)# code an argument here
-  words.find { |word| word.length > 4}
+  words.find { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
