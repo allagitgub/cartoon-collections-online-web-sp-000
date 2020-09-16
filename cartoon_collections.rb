8abb4e306b32133246adc067a5b195a3821860bd
Alla Kuzminova <alchik@gmail.com>
2020-09-16 01:52:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,14 @@ end
 def find_the_cheese(ingridients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  return ingridients.include?(cheese_types[0]) ||
-  ingridients.include?(cheese_types[1]) ||
-  ingridients.include?(cheese_types[2]) ? true : false
+
+  if ingridients.include?(cheese_types[0])
+    return cheese_types[0]
+  elsif ingridients.include?(cheese_types[1])
+    return cheese_types[1]
+  elsif ingridients.include?(cheese_types[2])
+    return cheese_types[2]
+  else
+    return nil
+ end  
 end
